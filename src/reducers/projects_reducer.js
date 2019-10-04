ff81b0e09c72ddae0f145152eebba7728139bb36
Mark Francis <markxf@gmail.com>
2019-10-04 18:45:12 +0100

    revise project list

--- src/reducers/projects_reducer.js
+++ src/reducers/projects_reducer.js
@@ -5,8 +5,8 @@ export default function(state = null, action) {
     default: {
       return [
         { index: '01', name: 'Inkscribe', url: '#', desc: 'Creative microblogging platform - coming soon', technology: 'MongoDB, Express, React, Node.js' },
-        { index: '02', name: 'Plantza', url: 'https://plantza-alan1010010.herokuapp.com/', desc: 'Plant rental marketplace; built during Le Wagon as part of a team of four developers', technology: 'Ruby on Rails, PostgreSQL, Heroku' },
-        { index: '03', name: 'Plantis', url: 'http://www.plantis.co/', desc: 'Social house-plant management system; built during Le Wagon as part of a team of four developers', technology: 'Ruby on Rails, PostgreSQL, Heroku' }
+        { index: '02', name: 'Plantza', url: 'https://plantza-alan1010010.herokuapp.com/', desc: 'Plant rental marketplace', technology: 'Ruby on Rails, PostgreSQL, Heroku' },
+        { index: '03', name: 'Plantis', url: 'http://www.plantis.co/', desc: 'Social house-plant management system', technology: 'Ruby on Rails, PostgreSQL, Heroku' }
       ];
     }
   }
